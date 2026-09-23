5462edef9cff5e7dc888c98b6a8e6da75dcce431
prajapati1010bharat <bbp03@ganpatuniversity.ac.in>
2026-09-23 17:27:39 +0530

    V4

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, UploadFile, File
 from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
@@ -38,6 +38,22 @@ templates = Jinja2Templates(directory="./templates")
 @app.get("/")  # Vist Home Page
 def home(request: Request):
     return templates.TemplateResponse(request, "home.html", {"courses": courses_list})
+
+
+# File Uploading - Load the View
+@app.get("/upload")
+def upload_file(request: Request):
+    return templates.TemplateResponse(request, "upload.html")
+
+
+# File Uploading - Process of file uploading....
+@app.post("/upload")
+async def upload_file(file: UploadFile = File(...)):
+    with open(file.filename, "wb") as f:
+        content = await file.read()
+        f.write(content)
+
+    return {"message": "File Uploaded succesfully...."}
 
 
 # @app.get("/contactus")  # Visit Contact Us Page
